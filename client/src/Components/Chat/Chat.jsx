bbecb46af5133062927e7af2cdbd34f50694f991
khushbookhator <khushbookhator11@gmail.com>
2021-07-03 14:37:09 +0530

    chat functionality added

--- client/src/Components/Chat/Chat.jsx
+++ client/src/Components/Chat/Chat.jsx
@@ -1,34 +1,71 @@
 import React, { useEffect, useState } from 'react'
 import queryString from "query-string"
 import io from "socket.io-client"
+import "./chat.css"
+import {InfoBar} from "./InfoBar"
+import {Input} from "./Input"
+import {Messages} from "./Messages"
 
+
+const ENDPOINT = 'localhost:2244'
 let socket;
 
 const Chat = ({location}) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    const ENDPOINT = 'localhost:2244'
+    const [users, setUsers] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
 
     useEffect(() => {
-        const {name, room} = queryString.parse(location.search)
-        socket = io(ENDPOINT)
+         const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setRoom(room);
         setName(name)
-        setRoom(room)
 
-        socket.emit('join', {name, room}, () => {
-            
-        })
+        socket.emit('join', { name, room }, (error) => {
+            if(error) {
+              alert(error);
+            }
+        });
 
-        return() => {
-            socket.emit('disconnect')
-            socket.off()
-        }
+        // return() => {
+        //     socket.emit('disconnect')
+        //     socket.off()
+        // }
         
     },[ENDPOINT, location.search])
 
+    useEffect(() => {
+        socket.on('message', message => {
+        setMessages(messages => [ ...messages, message ]);
+        });
+        
+        socket.on("roomData", ({ users }) => {
+        setUsers(users);
+        });
+    },[])
+
+    //function for sending message
+    const sendMessage = (event) => {
+        event.preventDefault();
+    
+        if(message) {
+          socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
+
     return (
-        <div>
-            
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room} />
+                <Messages messages={messages} name={name} />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+            </div>
+        {/* <TextContainer users={users}/> */}
         </div>
     )
 }
--- server2/index.js
+++ server2/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
 
+const {addUser, removeUser, getUser, getUserInRoom} = require("./user")
+
 const router = require('./router');
 
 const app = express();
@@ -12,15 +14,38 @@ app.use(cors());
 app.use(router);
 
 io.on('connect', (socket) => {
-    console.log("a new userhas connected")
-    socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room)
-
-    });
+    console.log("a new user has connected")
     
+    socket.on('join', ({ name, room }, callback) => {
+      const {error, user} = addUser({id: socket.id, name, room})
 
-  
+      if(error) return callback(error)
+      //admin generated message
+      socket.join(user.room)
+      socket.emit('message', {user: 'admin', text: `${user.name}, Welcome to ${user.room} group`})
+
+      socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined.`})
+
+
+      io.to(user.room).emit('roomData', {room: user.room, users: getUserInRoom(user.room)})
+
+      callback()
+    });
+    //User Generated Message
+    socket.on('sendMessage', (message, callback) => {
+      const user = getUser(socket.id);
+
+      io.to(user.room).emit('message', {user: user.name, text: message})
+      
+      callback()
+      
+    })  
     socket.on('disconnect', () => {
+      const user = removeUser(socket.id)
+      if(user){
+        io.to(user.room).emit('message', {user: 'admin', text: `${user.name} has left`})
+        io.to(user.name).emit('roomData', {room: user.room, users: getUserInRoom(user.room)})
+      }
       console.log("disconnected")
     })
 });
